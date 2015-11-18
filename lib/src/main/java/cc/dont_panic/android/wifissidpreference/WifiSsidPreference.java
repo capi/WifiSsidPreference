94bb0d31eaf9ad93d0def7af906b3c7a550d28dc
Martin Carpella <martin.carpella@gmx.net>
2015-11-18 21:06:02 +0100

    Minimize host application

---
+++ lib/src/main/java/cc/dont_panic/android/wifissidpreference/WifiSsidPreference.java
@@ -0,0 +1,14 @@
+package cc.dont_panic.android.wifissidpreference;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+public class WifiSsidPreference extends DialogPreference {
+
+
+    public WifiSsidPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
